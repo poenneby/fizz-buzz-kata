1fa9d8ec6d85579b417fa01457a3b91b9a4859bc
Peter Onneby <ponneby@xebia.fr>
2018-06-20 13:53:57 +0200

    Printing numbers up to 15

--- Java/src/main/java/FizzBuzz.java
+++ Java/src/main/java/FizzBuzz.java
@@ -3,7 +3,11 @@ public class FizzBuzz {
         if (args.length > 0) {
             int count = Integer.parseInt(args[0]);
             for (int i = 0; i<count; i++) {
-                System.out.print(String.format("%s\n", i+1));
+                if (i+1 < count) {
+                    System.out.print(String.format("%s\n", Converter.convert(i+1)));
+                } else {
+                    System.out.print(Converter.convert(i+1));
+                }
             }
         } else {
             System.out.print("1");
--- Java/src/test/java/FizzBuzzTest.java
+++ Java/src/test/java/FizzBuzzTest.java
@@ -28,4 +28,26 @@ public class FizzBuzzTest {
         FizzBuzz.main("2");
         assertThat(output.toString()).isEqualTo("1\n2");
     }
+
+    @Test
+    public void should_print_first_15_numbers() throws Exception {
+        FizzBuzz.main("15");
+        assertThat(output.toString()).isEqualTo(
+                "1\n" +
+                "2\n" +
+                "Fizz\n" +
+                "4\n" +
+                "Buzz\n" +
+                "Fizz\n" +
+                "7\n" +
+                "8\n" +
+                "Fizz\n" +
+                "Buzz\n" +
+                "11\n" +
+                "Fizz\n" +
+                "13\n" +
+                "14\n" +
+                "FizzBuzz"
+        );
+    }
 }
