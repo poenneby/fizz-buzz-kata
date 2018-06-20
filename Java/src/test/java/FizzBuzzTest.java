18b6f51409358605a3a8079adac6236970fff085
Peter Onneby <ponneby@xebia.fr>
2018-06-20 13:44:58 +0200

    Remove duplication in tests

--- Java/src/test/java/FizzBuzzTest.java
+++ Java/src/test/java/FizzBuzzTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -7,25 +8,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
-    @Test
-    public void should_print_numbers() throws Exception {
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private ByteArrayOutputStream output;
+
+    @Before
+    public void prepare_streams() {
+        output = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(output);
         System.setOut(printStream);
+    }
 
+    @Test
+    public void should_print_numbers() throws Exception {
         FizzBuzz.main();
-
         assertThat(output.toString()).isEqualTo("1");
     }
 
     @Test
     public void should_print_a_given_amount_of_numbers() throws Exception {
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(output);
-        System.setOut(printStream);
-
         FizzBuzz.main("2");
-
-        assertThat(output.toString()).isEqualTo("1\n2\n");
+        assertThat(output.toString()).isEqualTo("1\n2");
     }
 }
